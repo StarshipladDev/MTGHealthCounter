3accc2a2bc283f1c0821ee480df164590b394be8
StarshipladDev <StarshipladDev@gmail.com>
2019-10-08 23:23:15 +1300

    Non-working Counter upload
    08/10/19 Added simple 'add counter' buttons and relevant counter handling in-app
    CHANGES
    -----------------
    Gave activity_main an id for referencing when adding Image-views
    Added 3 Counter images representing 1/1 , 2/2 and 3/3 creature tokens
    Added a 'Add COunter' Button Image (Unsued)
    Added 2 'add Counter Button objects, 1 per player, in Main View
    Update Readme
    Added a counter listener that creates a new image view on click that has a swipe-listener
    Added a swipe-listener class that takes an image-view and changes that view's image on certain swipe.
    
    To Do
    ----------------
    Implement 4 counters per player, formatted correctly in a UI friendly manner.
    Implement counter swiping correctly
    Add a help button
    Begin correct documentation in the log, change its format to PDF
    
    NEXT BUILD WEEK ENDING 13/10/19

--- MainActivity.java
+++ MainActivity.java
@@ -2,6 +2,7 @@ package com.example.healtcount;
 
 import android.graphics.Color;
 import android.media.Image;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,32 +11,41 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.media.MediaPlayer;
 import android.os.PowerManager;
-
+import android.view.animation.*;
 import java.util.Random;
 import java.util.Timer;
+import android.content.Context;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
 
 public class MainActivity extends AppCompatActivity {
     static String TAG="MainActivity";
     String player1="Starshiplad";
     String player2="Starshiplass";
+    static int Health1=20;
+    static int Health2=20;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-
         Log.i(TAG," Yay the program is working!");
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
         //Taken from https://stackoverflow.com/questions/5712849/how-do-i-keep-the-screen-on-in-my-app
+        //Keeps phone from turning off
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         //End of stolen code
-        //Draw iamge before others so other draw over it
+        //Draw image before others so other items draw over it
         ImageView bg = (ImageView)(findViewById(R.id.Background));
         bg.setImageResource(getResources().getIdentifier("@drawable/background",null,this.getPackageName()));
-        //Add buttons with attatched Images
+        //Add/initialize buttons with attached Images
         ImageView AddButton1 = (ImageView)(findViewById(R.id.AddButton1));
         AddButton1.setImageResource(getResources().getIdentifier("@drawable/plus",null,this.getPackageName()));
         ImageView SubtractButton1 = (ImageView)(findViewById(R.id.SubtractButton1));
@@ -46,25 +56,41 @@ public class MainActivity extends AppCompatActivity {
         SubtractButton2.setImageResource(getResources().getIdentifier("@drawable/minus",null,this.getPackageName()));
         ImageView coinToss= (ImageView)(findViewById(R.id.CoinToss));
         coinToss.setImageResource(getResources().getIdentifier("@drawable/coinhead",null,this.getPackageName()));
+        Button addCounter1 = (Button)(findViewById(R.id.addCounter1));
+        Button addCounter2 = (Button)(findViewById(R.id.addCounter2));
         //Creates a media sound effect to play a 'Blood letting' sound on minus health
         MediaPlayer mp1 = MediaPlayer.create(this, R.raw.splat);
         MediaPlayer mp2 = MediaPlayer.create(this, R.raw.shine);
         MediaPlayer mp3 = MediaPlayer.create(this, R.raw.coin);
-        //Add text veiws of scores and then create listeners
+        //Add text veiws of scores & counter button ,rotate score 2 and counter 2
         TextView h1 = (TextView)(findViewById(R.id.HealthText1)) ;
-        h1.setTextColor(Color.WHITE);
-        ButtonListener b1 = new ButtonListener(h1,player1,mp1,mp2,mp3,coinToss,this);
         TextView h2 = (TextView)(findViewById(R.id.HealthText2)) ;
+        RotateAnimation rotate= (RotateAnimation)AnimationUtils.loadAnimation(this,R.anim.animation);
+        h2.setAnimation(rotate);
+        //addCounter2.setAnimation(rotate);
+        h1.setTextColor(Color.WHITE);
         h2.setTextColor(Color.WHITE);
+        //Create Buton Listeners
+        ButtonListener b1 = new ButtonListener(h1,player1,mp1,mp2,mp3,coinToss,this);
         ButtonListener b2 = new ButtonListener(h2,player2,mp1,mp2,mp3,coinToss,this);
-
-        //Attatch button listners
+        CounterListner countList = new CounterListner();
+        View.OnLongClickListener lc1 = new HoldListener(h1,player1,mp1,mp2);
+        View.OnLongClickListener lc2 = new HoldListener(h2,player2,mp1,mp2);
+        //Attach button listeners
         AddButton1.setOnClickListener(b1);
         SubtractButton1.setOnClickListener(b1);
+        AddButton1.setOnLongClickListener(lc1);
+        SubtractButton1.setOnLongClickListener(lc1);
         AddButton2.setOnClickListener(b2);
         SubtractButton2.setOnClickListener(b2);
+        AddButton2.setOnLongClickListener(lc2);
+        SubtractButton2.setOnLongClickListener(lc2);
         coinToss.setOnClickListener(b2);
-
+        addCounter1.setOnClickListener(countList);
+        addCounter2.setOnClickListener(countList);
+        //Call final display parameters
+        h1.setText(player1+"'s Health: "+ Health1 + "\n");
+        h2.setText(player2+"'s Health: "+ Health2 + "\n");
     }
     protected void onDestroy(){
 
@@ -79,11 +105,101 @@ public class MainActivity extends AppCompatActivity {
             iV.setImageResource(R.drawable.coinhead);
         }
     }
-    private class ButtonListener implements View.OnClickListener{
-        int number=20;
+    private class HoldListener implements View.OnLongClickListener {
+        int number;
+        String player;
         TextView t;
-        MediaPlayer mp1;
-        MediaPlayer mp2;
+        MediaPlayer mploss;
+        MediaPlayer mphealth;
+
+        public HoldListener(TextView t, String player,MediaPlayer mploss,MediaPlayer mphealth) {
+            this.mploss=mploss;
+            this.mphealth=mphealth;
+            this.t = t;
+            this.player = player;
+            if(player.equals(player1)){
+                number=Health1;
+            }
+            else if(player.equals(player2)){
+                number=Health2;
+            }
+        }
+
+        @Override
+        public boolean onLongClick(View v) {
+            if(player.equals(player1)){
+                number=Health1;
+            }
+            else if(player.equals(player2)){
+                number=Health2;
+            }
+            Log.i(TAG, "onLongClick:Longclick true");
+            if(v.getId()==R.id.SubtractButton1 || v.getId()==R.id.SubtractButton2) {
+                if(number>0){
+                    mploss.start();
+                }
+                number=number-5;
+                if(number<0){
+                    number=0;
+                }
+            }
+            else{
+                mphealth.start();
+                number=number+5;
+            }
+            t.setText(player+"'s Health: "+ number + "\n");
+
+            if(player.equals(player1)){
+                Health1=number;
+            }
+            else if(player.equals(player2)){
+                Health2=number;
+            }
+            return true;
+        }
+    }
+    private class CounterListner implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            if(v.getId()==R.id.addCounter1){
+                Log.i(TAG,"Counter1");
+            }
+            else if(v.getId()==R.id.addCounter2){
+                ImageView counter = new ImageView(MainActivity.this);
+                counter.setImageResource(R.drawable.counter1);
+                ConstraintLayout relativeLayout = (ConstraintLayout) findViewById(R.id.overall);
+                RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
+                        RelativeLayout.LayoutParams.WRAP_CONTENT,
+                        RelativeLayout.LayoutParams.WRAP_CONTENT
+                );
+                layoutParams.addRule(RelativeLayout.ABOVE, R.id.addCounter2);
+                layoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+                layoutParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+                relativeLayout.addView(counter, layoutParams);
+                counter.setOnTouchListener(new OnSwipeTouchListener(MainActivity.this,counter){
+                    public void onSwipeTop() {
+                        this.iv.setImageResource(R.drawable.counter2);
+                    }
+                    public void onSwipeRight() {
+                        Log.i(TAG,"right");
+                    }
+                    public void onSwipeLeft() {
+                        Log.i(TAG,"left");
+                    }
+                    public void onSwipeBottom() {
+                        this.iv.setImageResource(R.drawable.counter3);
+                    }
+
+                });
+            }
+
+        }
+    }
+    private class ButtonListener implements View.OnClickListener{
+        int number;
+        TextView t;
+        MediaPlayer mploss;
+        MediaPlayer mphealth;
         MediaPlayer mp3;
         String player;
         ImageView iV;
@@ -92,19 +208,36 @@ public class MainActivity extends AppCompatActivity {
         public ButtonListener(TextView t,String player, MediaPlayer mp1, MediaPlayer mp2,MediaPlayer mp3,ImageView iV,AppCompatActivity c){
             this.t= t;
             this.player=player;
-            this.mp1=mp1;
-            this.mp2=mp2;
+            this.mploss=mp1;
+            this.mphealth=mp2;
             this.iV=iV;
             this.mp3=mp3;
             c=c;
+            if(player.equals(player1)){
+                number=Health1;
+            }
+            else if(player.equals(player2)){
+                number=Health2;
+            }
+
         }
         @Override
         public void onClick(View v) {
+            if(player.equals(player1)){
+                number=Health1;
+            }
+            else if(player.equals(player2)){
+                number=Health2;
+            }
+            Log.i(TAG," Buttonclick used");
+
+            t.setText(player+"'s Health: "+ number + "\n");
+            Log.i(TAG, "" + number + "\n");
+            //Taken from https://stackoverflow.com/questions/6151566/how-to-wait-a-thread-in-android
             if(v.getId()==R.id.SubtractButton1 || v.getId()==R.id.SubtractButton2) {
                 number--;
-                mp1.start();
+                mploss.start();
             }
-            //Taken from https://stackoverflow.com/questions/6151566/how-to-wait-a-thread-in-android
             else if(v.getId()==R.id.CoinToss){
                 if(coinSpinning==false){
                     coinSpinning=true;
@@ -113,7 +246,7 @@ public class MainActivity extends AppCompatActivity {
                         int i=0;
                         public void run(){
                             mp3.start();
-                            while(i<6){
+                            while(i<8){
                                 try{
                                     Thread.sleep(300);
                                 }
@@ -146,10 +279,84 @@ public class MainActivity extends AppCompatActivity {
             }
             else{
                 number++;
-                mp2.start();
+                mphealth.start();
+            }
+            if(player.equals(player1)){
+                Health1=number;
+            }
+            else if(player.equals(player2)){
+                Health2=number;
             }
             t.setText(player+"'s Health: "+ number + "\n");
             Log.i(TAG, "" + number + "\n");
+        }
+    }
+    //Taken from https://stackoverflow.com/questions/4139288/android-how-to-handle-right-to-left-swipe-gestures
+    private class OnSwipeTouchListener implements OnTouchListener {
+        public ImageView iv=null;
+        private final GestureDetector gestureDetector;
+
+        public OnSwipeTouchListener (Context ctx,ImageView iv){
+            gestureDetector = new GestureDetector(ctx, new GestureListener());
+            this.iv=iv;
+        }
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            return gestureDetector.onTouchEvent(event);
+        }
+
+        private final class GestureListener extends SimpleOnGestureListener {
+
+            private static final int SWIPE_THRESHOLD = 100;
+            private static final int SWIPE_VELOCITY_THRESHOLD = 100;
+
+            @Override
+            public boolean onDown(MotionEvent e) {
+                return true;
+            }
+
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                boolean result = false;
+                try {
+                    float diffY = e2.getY() - e1.getY();
+                    float diffX = e2.getX() - e1.getX();
+                    if (Math.abs(diffX) > Math.abs(diffY)) {
+                        if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                            if (diffX > 0) {
+                                onSwipeRight();
+                            } else {
+                                onSwipeLeft();
+                            }
+                            result = true;
+                        }
+                    }
+                    else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                        if (diffY > 0) {
+                            onSwipeBottom();
+                        } else {
+                            onSwipeTop();
+                        }
+                        result = true;
+                    }
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+                return result;
+            }
+        }
+
+        public void onSwipeRight() {
+        }
+
+        public void onSwipeLeft() {
+        }
+
+        public void onSwipeTop() {
+        }
+
+        public void onSwipeBottom() {
         }
     }
 }
--- README.md
+++ README.md
@@ -1,12 +1,65 @@
 # MTGHealthCounter
 This app is designed to provide a simple and friendly 2-player life counting app for games of Magic The Gathering.
+The rulesfor MTG can be found at https://magic.wizards.com/en/game-info/gameplay/rules-and-formats/rules
+
+#Skills To Develop
+This project is a designed to build on 3 key skills.
+
+My ability to work with android studio, and further build on my Java/XML skills
+
+My ability to document my work and git releases/ maintain a development schedule
+
+My skills in pixel art and user interfaces
+
+# Features
+** Current Features **:
+
+Disabled auto-power off
+
+2 palyers have UI's facing each of them
+
+Coin object in centre of screen allows computor to detirmine start player
+
+Players can add/subtract their owen health as game paly progresses
+
+** Planned Features **:
+
+Ability to add/remove various counters to represent token creatures/ +1/+1 counters
+
+Ability to quickly reset points
+
+Implementation of multiplayer games
+
+Implementation of Players being allowed to edit names
+
+# Latest Upload
+8/10/2019 - Unstable Counter implementation
+
+# Next build
+Week ending 13/10/2019 - 
+
+>Implement each player being able to add up to 4 counters, that can be swiped to change counter type
+
+>Add correct documentation similar to Five Move Murder Fest (https://github.com/StarshipladDev/FiveMoveMurder)
+
+>Release as unstable build in Full-screen branch
+
+>Bug fix for master merge and APK release week after
+
+# Most Recent Stable Build:
+Hold button health - Added full-screen support and holding buttons to increment by 5 HP
 
 # Installing and Compiling:
-There are currently plans to import this application into the Google Play app store, however, if you have a computer running  android studio, the following video explains how to load and run a program.(The app can be run remotley after) 	https://www.youtube.com/watch?v=y3nwpowsMWs
+There are currently plans to import this application into the Google Play app store, however, if you have a computer running  android studio, the following video explains how to load and run a program.(The app can be run remotely after) 	https://www.youtube.com/watch?v=y3nwpowsMWs
 
-#File location required:
+# File location creations required:
+
 **Resources**
-* colors/strings/styles * - *source*\app\src\main\res\values
-* icon * - *source*\app\src\main\res\relevantmipmap *resize as neccessary*
-*Shine.wav and splat.wav * -*source*\app\src\main\res\raw
-*Background.png* -*source*\app\src\main\res\drawable
+
+* colors/strings/styles * - * source *\app\src\main\res\values
+* icon * - *source*\app\src\main\res\relevantmipmap * resize as necessary *
+* Shine.wav and splat.wav * -*source*\app\src\main\res\raw
+* Background.png * -* source *\app\src\main\res\drawable
+
+
+
